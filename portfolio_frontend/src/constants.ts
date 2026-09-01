5abf1973b0cc3d63f4b8dfe63687ceb3ae302486
hernaneveronica8-hub <hernaneveronica8@gmail.com>
2026-09-02 00:57:56 +0800

    Fix testnet portfolio object id constant

--- portfolio_frontend/src/constants.ts
+++ portfolio_frontend/src/constants.ts
@@ -10,7 +10,7 @@
 export const TESTNET_PACKAGE_ID = "0x1a855044bd43f7b8a01dc3030c19ce5a377d8e6a2afce9a72ac1a43fdf80dce4";
 
 // ✅ Your NEW Testnet Portfolio Object ID
-export const TESTNET_PORTFOLIO_ID = "0xcbf09e9c656e42a27000812cf4a974ca0b0b055c4f19aa1debaf5cc7f64ffdf6";
+export const TESTNET_PORTFOLIO_ID = "0xcbf09e9c656e42a27008812cf4a974ca8b0b855c4f19aa1debaf5cc7f64ffdf6";
 
 export const MAINNET_PACKAGE_ID = "0x0";
 export const MAINNET_PORTFOLIO_ID = "0xcbf09e9c656e42a27008812cf4a974ca8b0b855c4f19aa1debaf5cc7f64ffdf6";
